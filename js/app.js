8e82ec2c732717e87f51adda5598e6fb69b7f98f
Johannes Koggdal <koggdal@spotify.com>
2012-02-21 13:38:38 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+"use strict";
+var sp = getSpotifyApi(1);
+var models = sp.require('sp://import/scripts/api/models');
+var application = models.application;
+
+
+/*application.observe(models.EVENT.LINKSCHANGED, function () {
+
+});*/
